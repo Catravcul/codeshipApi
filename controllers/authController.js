692ae5c8730a19517fa3774d72d45d9eef96433d
sayeed <sayeed0209@gamil.com>
2020-11-18 18:41:44 +0100

    user sign up functionality

---
+++ controllers/authController.js
@@ -0,0 +1,18 @@
+const User = require('./../models/userModel');
+// console.log(User);
+exports.signup = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+};
