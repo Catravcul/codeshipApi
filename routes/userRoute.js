9885224bdb9beb7bcd0f9a811f54f171b8b5eed9
sayeed <sayeed0209@gamil.com>
2020-11-19 11:34:01 +0100

    route for login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 // http://127.0.0.1:5000/user/signup (signup routes)
 router.post('/signup', authController.signup);
+// http://127.0.0.1:5000/user/login   (login routes)
+router.post('/login', authController.login);
 
 module.exports = router;
