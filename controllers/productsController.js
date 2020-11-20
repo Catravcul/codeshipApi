34af96ae64ebad072f9dfc2485b743d1f4f45c45
sayeed <sayeed0209@gamil.com>
2020-11-20 12:44:30 +0100

    update,delete,getProduct by id functionality.

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,6 +1,6 @@
 const Products = require('./../models/productsModel');
 console.log(Products);
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = async (req, res, next) => {
   try {
     const products = await Products.find();
     // console.log(products);
@@ -13,21 +13,20 @@ exports.getAllProducts = async (req, res) => {
       status: 'fail',
       message: err.message,
     });
-    console.log(err.message)
+    next();
   }
-
 };
 
 exports.createProdcuts = async (req, res, next) => {
   try {
     const newProduct = await Products.create(req.body);
-    console.log(newProduct);
+    // console.log(newProduct);
     return res.status(200).json({
       status: 'success',
       data: { product: newProduct },
     });
   } catch (err) {
-    return res.status(400).json({
+    res.status(400).json({
       status: 'fail',
       message: err.message,
     });
@@ -35,3 +34,53 @@ exports.createProdcuts = async (req, res, next) => {
 
   next();
 };
+exports.getProductById = async (req, res, next) => {
+  try {
+    const product = await Products.findById(req.params.id);
+    return res.status(200).json({
+      status: 'OK',
+      data: product,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.updateProduct = async (req, res, next) => {
+  try {
+    const updateProduct = await Products.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json({
+      status: 'OK',
+      data: updateProduct,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+ exports.deleteProduct= async (req, res, next) => {
+     try{
+         const product = await Products.findByIdAndDelete(req.params.id);
+         res.status(204).json({
+             status: 'deleted',
+             data:null,
+         })
+     }catch (err) {
+        res.status(500).json({
+          message: err.message,
+          status: 'Internal Server Error',
+        });
+      }
+ }
