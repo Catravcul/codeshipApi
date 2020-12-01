d878c3b3a7c28d1638cbeb2d35a3b399da8ae233
beron <bleink@outlook.es>
2020-12-01 17:39:11 +0100

    userModel.js - fixed required word which hadn't the "d" and make email required, add minlength to name, username & lastname

--- models/userModel.js
+++ models/userModel.js
@@ -5,30 +5,34 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     maxlength: [15, 'A username should not be longer than 15 characters.'],
-    require: [true, 'A user should have a username.'],
-    unique: [true, 'A user should have a unique username.'],
+    minlength: [3, 'A username should not be shorter than 3 characters.'],
+    required: [true, 'A user should have a username.'],
+    unique: [true, 'A user should have a unique username.']
   },
   name: {
     type: String,
     maxlength: [12, 'A name should not be longer than 12 characters.'],
-    require: [true, 'A user should have a name.'],
+    minlength: [3, 'A name should not be shorter than 3 characters.'],
+    required: [true, 'A user should have a name.']
   },
   lastname: {
     type: String,
     maxlength: [15, 'A lastname should not be longer than 15 characters.'],
-    require: [true, 'A user should have a lastname.'],
+    minlength: [3, 'A lastname should not be shorter than 3 characters.'],
+    required: [true, 'A user should have a lastname.']
   },
   email: {
     type: String,
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, 'Please enter a valid email'],
+    required: [true, 'The email is required to know you are real'],
+    validate: [validator.isEmail, 'Please enter a valid email']
   },
   password: {
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 6,
-    select: false, // to hide password field
+    select: false // to hide password field
   },
   passwordConfirm: {
     type: String,
@@ -38,7 +42,7 @@ const userSchema = new mongoose.Schema({
       validator: function (el) {
         return el === this.password;
       },
-      message: 'Password are not the same',
+      message: 'Password are not the same'
     },
   },
   description: {
@@ -47,11 +51,11 @@ const userSchema = new mongoose.Schema({
     maxlength: [250, 'A user description must be less than 250 characters.']
   },
   img_path: {
-    type: String,
+    type: String
   },
   points: {
     type: Number,
-    default: 0,
+    default: 0
   }
 });
 // middleware hook
