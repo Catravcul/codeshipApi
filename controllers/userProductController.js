0b74b5017097b1639ebabbb0ab28e24c447dad2c
beron <bleink@outlook.es>
2020-12-02 20:03:15 +0100

    userProductController.js - add func to store new & show by owner id

---
+++ controllers/userProductController.js
@@ -0,0 +1,33 @@
+const UserProduct = require('../models/userProductModel');
+exports.show = async (req, res, next) => {
+    try {
+      const products = await UserProduct.find({owner: req.body.id})
+      return res.status(200).json({
+        status: 'OK',
+        data: { products: products },
+      })
+    } catch (err) {
+      res.status(404).json({
+        status: 'fail',
+        message: err.message,
+      })
+      next();
+    }
+}
+    
+exports.store = async (req, res, next) => {
+  try {
+    const newProduct = await UserProduct.create(req.body)
+    return res.status(200).json({
+      status: 'success',
+      data: { product: newProduct },
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    })
+  }
+
+  next()
+}
