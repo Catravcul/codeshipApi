12c43d5b630a29c847c0efef3b46e531121a44e0
beron <bleink@outlook.es>
2020-12-03 20:02:03 +0100

    app.js - port for cors static

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 console.log(process.env.PORT)
-app.use(cors({ origin: 'http://localhost:' + process.env.PORT || 5000}));
+app.use(cors({ origin: 'http://localhost:5000'}));
 
 app.use(express.json());
 app.use(express_fileupload({
