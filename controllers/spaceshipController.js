ce75e38ef10ca42231182031076a129e04c49e4e
beron <bleink@outlook.es>
2020-12-04 13:28:45 +0100

    spaceship[Controller, Route].js - add destroy route which use destroy func to remove all documents from the collection

--- controllers/spaceshipController.js
+++ controllers/spaceshipController.js
@@ -1,39 +1,38 @@
-const Spaceship = require('./../models/spaceshipModel');
+const Spaceship = require('./../models/spaceshipModel')
 
 exports.store = async (req, res, next) => {
   try {
-    const newSpaceship = await Spaceship.create(req.body);
+    const newSpaceship = await Spaceship.create(req.body)
     return res.status(200).json({
       status: 'success',
       data: { spaceship: newSpaceship },
-    });
+    })
   } catch (err) {
     res.status(400).json({
       status: 'fail',
       message: err.message,
-    });
+    })
   }
 
   next();
-};
+}
 
 exports.show = async (req, res, next) => {
   try {
-    const spaceship = await Spaceship.findById(req.params.id);
+    const spaceship = await Spaceship.findById(req.params.id)
     return res.status(200).json({
       status: 'OK',
       data: {spaceship: spaceship},
-    });
+    })
   } catch (err) {
     res.status(400).json({
       status: 'fail',
       message: err.message,
-    });
+    })
   }
-};
+}
 
 exports.update = async (req, res, next) => {
-  console.log(req.params.id)
   try {
     const updated = await Spaceship.findByIdAndUpdate(
       req.params.id,
@@ -42,15 +41,33 @@ exports.update = async (req, res, next) => {
         new: true,
         runValidators: true,
       }
-    );
+    )
     res.status(200).json({
       status: 'OK',
       data: {spaceship: updated},
-    });
+    })
   } catch (err) {
     res.status(400).json({
       status: 'fail',
       message: err.message,
-    });
+    })
   }
-};
+}
+
+exports.destroy = async (req, res, next) => {
+  let result;
+  try{
+    result = await Spaceship.remove({}, err => {
+      return err
+    })
+    res.status(200).json({
+      status: 'OK',
+      data: {result: result},
+    })
+  } catch(err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    })
+  }
+}
--- routes/spaceshipRoute.js
+++ routes/spaceshipRoute.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const SpaceshipController = require('./../controllers/spaceshipController');
-const router = express.Router();
+const express = require('express')
+const SpaceshipController = require('./../controllers/spaceshipController')
+const router = express.Router()
 
 router
   .route('/')
-  .post(SpaceshipController.store);
+  .post(SpaceshipController.store)
+  .delete(SpaceshipController.destroy)
 router.route('/:id')
 .get(SpaceshipController.show)
-.patch(SpaceshipController.update);
+.patch(SpaceshipController.update)
 
-module.exports = router;
+module.exports = router
