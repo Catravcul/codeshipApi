0744c9c3e8fd8ac1211141a3b05558df471f4616
beron <bleink@outlook.es>
2020-12-10 17:22:20 +0100

    server.js - add database for dev mode

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
-const app = require('./app');
-require('dotenv').config();
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const mongoose = require('mongoose')
+const app = require('./app')
+require('dotenv').config()
+
+let DB = process.env.DATABASE_LOCAL
+if( process.env.NODE_ENV === 'production' ){
+  DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+  )
+}
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -13,8 +17,8 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => console.log('connection successfull'))
-  .catch((err) => console.log(err.message));
-const PORT = process.env.PORT || 5000;
+  .catch((err) => console.log(err.message))
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`http://127.0.0.1:${PORT}`);
-});
+  console.log(`http://127.0.0.1:${PORT}`)
+})
