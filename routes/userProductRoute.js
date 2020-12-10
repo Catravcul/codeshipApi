f110929b3d95d3671512af82b12635432a556ce5
beron <bleink@outlook.es>
2020-12-10 17:38:27 +0100

    userProductRoute.js - [define,export][publicRoute, route]

--- routes/userProductRoute.js
+++ routes/userProductRoute.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const UserProductController = require('./../controllers/userProductController');
-const router = express.Router();
+const express = require('express')
+const {store, drop, show} = require('./../controllers/userProductController')
+const router = express.Router()
+const publicRouter = express.Router()
 
 router
   .route('/')
-  .post(UserProductController.store)
-  .delete(UserProductController.destroy)
-router.route('/:id')
-  .get(UserProductController.show);
+  .put( store )
+  .delete( drop )
+publicRouter.route('/:userId')
+  .get( show )
 
-module.exports = router;
+module.exports = {
+  userProductRoute: router,
+  userProductPublicRoute: publicRouter
+}
