c8d56901d8e8780626f515d465609cc962f9c276
beron <bleink@outlook.es>
2020-12-11 20:18:57 +0100

    commentController.js - return comments along with its owner data

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -20,10 +20,17 @@ exports.store = async (req, res, next) => {
 
 exports.showAll = async (req, res, next) => {
   try {
+    const User = require('../models/userModel')
     const comments = await Comment.find(req.body)
+    const result = [];
+    for(const commentObj of comments){
+      const {img_path, username, _id: user_id} = await User.findById(commentObj.owner)
+      const {comment} = commentObj
+      result.push({comment, img_path, username, user_id})
+    }
     return res.status(200).json({
       status: 'OK',
-      data: {comment: comments},
+      data: {comment: result},
     });
   } catch (err) {
     res.status(400).json({
