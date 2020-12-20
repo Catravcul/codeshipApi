e3bfbe2dc33a6593430fb5790dc6383930fca4ac
beron <bleink@outlook.es>
2020-12-20 22:33:21 +0100

    spaceshipController.js - censor data returned by functions pending on the goal, erased [config, components]_path from store func

--- controllers/spaceshipController.js
+++ controllers/spaceshipController.js
@@ -1,10 +1,15 @@
 const Spaceship = require('./../models/spaceshipModel')
 
 exports.index = async (req, res) => {
-  try{
+  try {
     const spaceships = await Spaceship.find()
+    const datas = []
+    spaceships.forEach(spaceship => {
+      const {owner, config, ...data} = spaceship
+      datas.push(data)
+    })
     return res.status(200).json({
-      spaceships: spaceships
+      spaceships: datas
     })
   } catch (err) {
     res.status(400).json({
@@ -16,11 +21,10 @@ exports.index = async (req, res) => {
 exports.store = async (req, res) => {
   try {
     req.body.owner = req.id
-    req.body.config_path = req.id + 'spaceship.js'
-    req.body.components_path = req.id + 'components.js'
     const newSpaceship = await Spaceship.create(req.body)
+    const {owner, config, ...data} = newSpaceship
     return res.status(200).json({
-      spaceship: newSpaceship
+      spaceship: data
     })
   } catch (err) {
     res.status(400).json({
@@ -31,9 +35,11 @@ exports.store = async (req, res) => {
 
 exports.show = async (req, res, next) => {
   try {
-    const spaceship = await Spaceship.findById(req.params.id)
+    const id = req.params.id || req.id
+    const spaceship = await Spaceship.findOne({owner: id})
+    if(req.params.id) var {owner, config, ...data} = spaceship
     return res.status(200).json({
-      spaceship: spaceship
+      spaceship: data || spaceship
     })
   } catch (err) {
     res.status(400).json({
@@ -46,12 +52,14 @@ exports.update = async (req, res, next) => {
   try {
     let updated
     if (req.params.id) {
-      const updated = await Spaceship.findByIdAndUpdate(
-        req.params.id, req.body, {new: true, runValidators: true,}
+      const {owner, config, ...body} = req.body
+      updated = await Spaceship.findByIdAndUpdate(
+        req.params.id, body, {new: true, runValidators: true,}
       )
     } else {
+      const {owner, ...body} = req.body
       updated = await Spaceship.findOneAndUpdate(
-        {owner: req.id}, req.body, {new: true, runValidators: true}
+        {owner: req.id}, body, {new: true, runValidators: true}
       )
     }
     res.status(200).json({
@@ -66,7 +74,7 @@ exports.update = async (req, res, next) => {
 
 exports.drop = async (req, res, next) => {
   let result;
-  try{
+  try {
     result = await Spaceship.remove({}, err => {
       return err
     })
